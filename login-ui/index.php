18ad3050422fc1ecc7da315359c314890238cb7e
Neha Jha <nehajha8720@gmail.com>
2022-07-14 09:22:13 +0530

    first commit

---
+++ login-ui/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Examinee LOGIN</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="login-ui/image/png" href="images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/bootstrap/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" type="text/css" href="login-ui/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/css/main.css"> -->
+	<link rel="stylesheet" type="text/css" href="login-ui/css/login.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- <link rel="stylesheet" type="text/css" href="main.css"> -->
+</head>
+<body>
+	
+	<!-- <div class="limiter">
+		<div class="container-login100 bg-info">
+			<div class="wrap-login100 bg-warning"><br>
+				<div>
+					<center><h1>Online Examination System</h1></center>
+				</div>
+
+				<form method="post" id="examineeLoginFrm" class="login100-form validate-form">
+					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+						<span class="label-input100">Email</span>
+						<input class="input100" type="text" name="username" placeholder="Enter email">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="pass" placeholder="Enter password">
+						<span class="focus-input100"></span>
+					</div>
+
+
+					<div class="container-login100-form-btn" align="right">
+						<button type="submit" class="login100-form-btn">
+							Login
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div> -->
+	<section class="login_page">
+        <div class="container-fluid mh-100">
+            <div class="row login_mob_top_wave">
+                <img class="ww-100 p-0" src="login-ui/images/icons/mobile-top-wave.svg" alt="">
+            </div>
+            <!-- <div class="row justify-content-center psr_display_1 psr_text_600">
+                PP Future Institute
+            </div> -->
+            <div class="login_mid_design row mb-4">
+                <div class="row justify-content-center psr_display_1 psr_text_600">
+                    PP Future Institute
+                </div>
+                <div class="col-md-6">
+                    <img class="login_design_img mt-4 mt-md-0" src="login-ui/images/icons/login-design.svg" alt="">
+                </div>
+                <div class="col-md-6">
+                <div class="d-flex flex-column align-items-center p-0">
+                    <img class="login_girl_user my-4" src="login-ui/images/icons/girl-user.svg" alt="">
+                
+                    <form class="login_form d-flex flex-column align-items-center p-0" method="post" id="examineeLoginFrm" class="login100-form validate-form">
+                        <div class="psr_form_box p-0">
+                        <img class="psr_tf_icon_l" src="login-ui/images/icons/person-fill.svg" alt="">
+                        <input class="form-control psr_tf_icon" placeholder="Email address…" type="text" name="username">
+                        </div>
+                        <div class="psr_form_box p-0 my-3">
+                        <img class="psr_tf_icon_l" src="login-ui/images/icons/lock-fill.svg" alt="">
+                        <input class="form-control psr_tf_icon" placeholder="password" type="password" name="pass">
+                        </div>
+                        <button class="btn btn-default login100-form-btn" type="submit">Log In</button>
+                    </form>
+                    <div class="w-100 d-flex justify-content-around mt-2 psr_text_gray">
+                        <a href="#">forgot password?</a>
+                        <a href="#">Sign UP</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="login_mob_btm_wave row bottom">
+                <img class="ww-100 p-0" src="login-ui/images/icons/mobile-bottom-wave.svg" alt="">
+            </div>
+        </div>
+        <div class="login_dsk_bg">
+            <img src="login-ui/images/icons/back_design.svg" alt="">
+        </div>
+        <div class="full_div"></div>
+    </section>
+	
+	<script src="login-ui/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="login-ui/vendor/animsition/js/animsition.min.js"></script>
+	<script src="login-ui/vendor/bootstrap/js/popper.js"></script>
+	<script src="login-ui/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="login-ui/vendor/select2/select2.min.js"></script>
+	<script src="login-ui/vendor/daterangepicker/moment.min.js"></script>
+	<script src="login-ui/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="login-ui/vendor/countdowntime/countdowntime.js"></script>
+	<script src="login-ui/js/main.js"></script>
+
+</body>
+</html>
---
+++ pages/home.php
@@ -0,0 +1,18 @@
+
+
+<div class="app-main__outer">
+    <div id="refreshData">
+            
+    </div>
+
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="psr_display_2 psr_text_400">
+                PP FUTURE INSTITUTE
+            </div>
+        </div>
+        <div class="row justify-content-center p-md-5 p-1 mt-5 mt-md-0">
+            <img class="main_exam_home_img m-md-5" src="assets/images/svg/online_exam.svg" alt="Online test">
+        </div>
+
+    </div>
